f5b94b8776459843f9e5cba84b18289f55acaf5c
INOCcent-afk <michael828inoc@gmail.com>
2020-12-08 13:35:15 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper(".swiper-container", {
+  speed: 400,
+
+  //   slidesPerView: 1.1,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    // 400: {
+    //   slidesPerView: 1.4,
+    // },
+  },
+});
